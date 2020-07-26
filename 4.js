708183bff8e6720ed38944a44b02597d0d3f2a4b
nabilaafifahqotrunnada <nabila_afifah_28rpl@student.smktelkom-mlg.sch.id>
2020-07-26 10:02:53 +0700

    Second Commit

--- 4.js
+++ 4.js
@@ -1,12 +1,23 @@
+//nilai awal loop
 let i = 1
+//total
 let sum = 0
+//batas loop
 let n = 10
+//nilai awal deret geometri
 let a = 4
+//rasio deret geometri
+let r = 3
 
+//akan eksekusi hingga kondisi false
 while(i <= n){
+    //penambhan nilai total
     sum = sum + a
-    a = a * 3
+    //perkalian dengan rasio
+    a = a * r
+    //increment nilai i
     i++
 }
 
+//tampilan nilai terakhir sum saat loop berhenti
 console.log(sum);
